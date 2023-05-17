218c07ef447decfa1f4f22582cdcbc938f34836c
Umair <umairwasim001@gmail.com>
2023-05-18 02:40:12 +0500

    Create AttackPlayer.cs

---
+++ MummyEscape/Assets/Scripts/Enemy/AttackPlayer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlayer : MonoBehaviour
+{
+
+    public void Hit(Vector3 point)
+    {
+        
+    }
+}
