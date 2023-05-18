1ff88c494bb5feb8922b509c9cad603034a0018b
Umair <umairwasim001@gmail.com>
2023-05-19 04:33:12 +0500

    Added Camera orbit script

---
+++ MummyEscape/Assets/Scripts/Utilities/CameraOrbit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 5f;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        //calculate rotate position 
+        float angle = speed * Time.time;
+        Vector3 rotatePosition = target.position + Quaternion.AngleAxis(angle, Vector3.up) * offset;
+        rotatePosition.y = transform.position.y;
+
+        //Move the camera to rorate position
+        transform.position = rotatePosition;
+
+        //camera look at target
+        transform.LookAt(target);
+    }
+}
